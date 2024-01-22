58b2a48f982751c22b9538e68536de06d16754b3
zcubbs <zackcubby@gmail.com>
2024-01-22 18:02:46 +0100

    :rocket: update clone path repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,26 @@
 # Configure Git credentials
 git config --global credential.helper 'cache --timeout=3600'
 echo "url=$GIT_REPO_URL
-username=$GIT_REPO_USERNAME
-password=$GIT_REPO_PASSWORD" | git credential approve
+username=$GIT_USERNAME
+password=$GIT_PASSWORD" | git credential approve
 
-# Check out the Git repository
-git clone "$GIT_REPO_URL" tmp-repo
-"cd tmp-repo"
+# Check out the Git repository into a directory named "tmp-repo"
+git clone $GIT_REPO_URL /tmp-repo
+
+# Exit if clone fails
+if [ $? -ne 0 ]; then
+    echo "Git clone failed. Exiting."
+    exit 1
+fi
+
+# Change to the cloned repository directory
+cd /tmp-repo
+
+# Exit if cd fails
+if [ $? -ne 0 ]; then
+    echo "Failed to change directory to /tmp-repo. Exiting."
+    exit 1
+fi
 
 # Run Ansible playbook
-ansible-playbook -i inventory "$PLAYBOOK_NAME" --vault-password-file <(echo "$ANSIBLE_VAULT_KEY")
+ansible-playbook -i inventory $PLAYBOOK_NAME --vault-password-file <(echo $ANSIBLE_VAULT_KEY)
