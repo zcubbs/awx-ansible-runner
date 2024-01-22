6f58d632dbd0c07bf68d0acf524360e064d0ec74
zcubbs <zackcubby@gmail.com>
2024-01-22 17:58:04 +0100

    :rocket: update clone path repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ username=$GIT_REPO_USERNAME
 password=$GIT_REPO_PASSWORD" | git credential approve
 
 # Check out the Git repository
-git clone "$GIT_REPO_URL"
-"cd $(basename "$GIT_REPO_URL")"
+git clone "$GIT_REPO_URL" tmp-repo
+"cd tmp-repo"
 
 # Run Ansible playbook
 ansible-playbook -i inventory "$PLAYBOOK_NAME" --vault-password-file <(echo "$ANSIBLE_VAULT_KEY")
