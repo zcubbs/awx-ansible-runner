b34dbad901fdd02487798e0df2c43ea3a9d0cf9d
zakaria <zakaria.elbouwab@berger-levrault.com>
2024-01-23 15:47:14 +0100

    :rocket: update entrypoint, add var check

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,5 +24,15 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+# Check variables are set $PLAYBOOK_NAME $ANSIBLE_VAULT_KEY $CONTROLLER_HOST
+# $CONTROLLER_USERNAME $CONTROLLER_PASSWORD
+if [ -z "$PLAYBOOK_NAME" ] || [ -z "$ANSIBLE_VAULT_KEY" ] || [ -z "$CONTROLLER_HOST" ] || [ -z "$CONTROLLER_USERNAME" ] || [ -z "$CONTROLLER_PASSWORD" ]; then
+    echo "One or more required variables are not set. Exiting. PLAYBOOK_NAME, ANSIBLE_VAULT_KEY, CONTROLLER_HOST, CONTROLLER_USERNAME, CONTROLLER_PASSWORD are required."
+    exit 1
+fi
+
+# Print CONTROLLER_HOST
+echo "CONTROLLER_HOST: $CONTROLLER_HOST"
+
 # Run Ansible playbook
 ansible-playbook -i inventory $PLAYBOOK_NAME --vault-password-file <(echo $ANSIBLE_VAULT_KEY)
